15e1532d4ae2ba87b4a524d82e80047420dc76d1
Alexander <alez.grante@gmail.com>
2022-05-30 16:58:11 +0300

    Initial commit

---
+++ src/main/kotlin/Spinner.kt
@@ -0,0 +1,58 @@
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Spinner() {
+
+    val tablesList = mutableListOf<String>("Персонал", "Табель", "Места", "Знакомые","Люди")
+
+    // State variables
+    var currentTable by remember { mutableStateOf(tablesList[0]) }
+    var expanded by remember { mutableStateOf(false)}
+
+    Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.TopCenter) {
+        Row(
+            Modifier
+                .padding(18.dp)
+                .clickable {
+                    expanded = !expanded
+                },
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) { // Anchor view
+            Text(text = currentTable,fontSize = 18.sp,modifier = Modifier.padding(end = 8.dp)) // Country name label
+            Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
+
+            //
+            DropdownMenu(expanded = expanded, onDismissRequest = {
+                expanded = false
+            }) {
+                tablesList.forEach{ country->
+                    DropdownMenuItem(onClick = {
+                        expanded = false
+                        currentTable = country
+                    }) {
+                        Text(text = country)
+                    }
+                }
+            }
+        }
+    }
+
+}
