3521b8618bbffde8115d27b5d308d982e96ad25d
Alexander <alez.grante@gmail.com>
2022-06-01 16:49:26 +0300

    Relatives completed

--- src/main/kotlin/data/db/models/Relative.kt
+++ src/main/kotlin/data/db/models/Relative.kt
@@ -1,5 +1,12 @@
 package data.db.models
 
-data class Relative (
-
-        )
+data class Relative(
+    val name: String,
+    val relationDegree: String,
+    val employment: String,
+    val birthDay: String,
+    val birthPlace: String,
+    val nationality: String,
+    val citizen: String,
+    val admissionForm: Int,
+)
--- src/main/kotlin/data/db/tables/RelativesTable.kt
+++ src/main/kotlin/data/db/tables/RelativesTable.kt
@@ -9,14 +9,21 @@ object RelativesTable : IntIdTable() {
     val name: Column<String> = varchar("name", 20)
     val relationDegree: Column<String> = varchar("relationDegree", 30)
     val employment: Column<String> = varchar("employment", 40)
-    val birthDay: Column<Data> = float("weight")
-    val address: Column<Int> = integer("address_id")
+    val birthDay: Column<String> = varchar("birthDay", 20)
+    val birthPlace: Column<String> = varchar("birthPlace", 40)
+    val nationality: Column<String> = varchar("nationality", 40)
+    val citizen: Column<String> = varchar("citizen", 40)
+    val admissionForm: Column<Int> = integer("admissionForm")
 
     fun toRelative(row: ResultRow) = Relative(
         name = row[name],
-        age = row[age],
-        weight = row[weight],
-        address = row[address]
+        relationDegree = row[relationDegree],
+        employment = row[employment],
+        birthDay = row[birthDay],
+        birthPlace = row[birthPlace],
+        nationality = row[nationality],
+        citizen = row[citizen],
+        admissionForm = row[admissionForm],
     )
 
 }
