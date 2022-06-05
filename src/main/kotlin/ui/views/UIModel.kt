b1ed06f221d005e71e4ca1a3553393209f13926c
Alexander <alez.grante@gmail.com>
2022-06-05 10:59:18 +0300

    refac

--- src/main/kotlin/ui/views/UIModel.kt
+++ src/main/kotlin/ui/views/UIModel.kt
@@ -5,15 +5,10 @@ import org.jetbrains.exposed.sql.Table
 data class UIModel(val params: List<Pair<String, String>>) {
 
     companion object {
-        fun emptyModel(table: Table): UIModel {
-
-            val list = mutableListOf<Pair<String, String>>()
-
-            table.columns.drop(1).forEach {
-                list.add(it.name to "")
+        fun emptyModel(table: Table): UIModel = UIModel(
+            table.columns.drop(1).map {
+                it.name to ""
             }
-
-            return UIModel(list.toList())
-        }
+        )
     }
 }
